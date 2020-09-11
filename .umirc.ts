67805e352a28fd322682811d82303f196d4a1145
HeskeyBaozi <hezhiyu233@foxmail.com>
2020-09-11 10:38:43 +0800

    add deps

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'dumi';
 
 const PUBLIC_PATH = '/formular/';
+const FONT_FAMILY = `'Fira Code',Consolas,Monaco,"Andale Mono","Ubuntu Mono",monospace`;
 
 export default defineConfig({
   mode: 'site',
@@ -15,19 +16,25 @@ export default defineConfig({
     },
   ],
   styles: [
-    `code[class*=language-][class*=language-], pre[class*=language-][class*=language-] {
-  font-family: 'Fira Code',Consolas,Monaco,"Andale Mono","Ubuntu Mono",monospace;
+    `code[class*=language-][class*=language-], pre[class*=language-][class*=language-], code {
+  font-family: ${FONT_FAMILY};
   font-size: 14px;
+}`,
+    `pre, code, kbd, samp {
+  font-size: 1em;
+  font-family: ${FONT_FAMILY} !important;
 }`,
   ],
   base: PUBLIC_PATH,
   publicPath: PUBLIC_PATH,
+  outputPath: process.env.OUTPUT_PATH ?? './dist',
   theme: {
     '@c-primary': '#747474',
   },
   navs: {
     'en-US': [
       { title: 'Guide', path: '/guide' },
+      { title: 'Examples', path: '/examples' },
       { title: 'API', path: '/api' },
       {
         title: 'GitHub',
@@ -36,6 +43,7 @@ export default defineConfig({
     ],
     'zh-CN': [
       { title: '教程', path: '/zh-CN/guide' },
+      { title: '示例', path: '/zh-CN/examples' },
       { title: 'API', path: '/zh-CN/api' },
       {
         title: 'GitHub 仓库',
